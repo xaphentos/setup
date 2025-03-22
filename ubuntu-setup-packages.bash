e94807dc27f0d181b676b20b40bf68cec30c7d6d
xaphentos <aaronfaminski@outlook.com>
2025-03-22 17:58:39 +0200

    Add files via upload

---
+++ ubuntu-setup-packages.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "installing programms on your computer it may take a while.."
+
+sudo apt install -y htop vlc git  php libapache2-mod-php php-cli php-mysql php-xml php-mbstring php-curl php-json php-zip gnome-shell-extensions gnome-tweaks neofetch htop i3 i3status alacritty python3
