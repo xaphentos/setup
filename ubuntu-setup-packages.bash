9cf5b62861f5847030d4b0bff21e25e15a92dbbe
xaphentos <aaronfaminski@outlook.com>
2025-05-01 15:31:31 +0300

    Update ubuntu-setup-packages.bash

--- ubuntu-setup-packages.bash
+++ ubuntu-setup-packages.bash
@@ -2,4 +2,4 @@
 
 echo "installing programms on your computer it may take a while.."
 
-sudo apt install -y htop vlc git  php libapache2-mod-php php-cli php-mysql php-xml php-mbstring php-curl php-json php-zip gnome-shell-extensions gnome-tweaks neofetch htop i3 i3status alacritty python3
+sudo apt install -y htop vlc git  php libapache2-mod-php php-cli php-mysql php-xml php-mbstring php-curl php-json php-zip gnome-shell-extensions gnome-tweaks neofetch htop i3 i3status alacritty python3 kitty cowsay code nano wget curl discord docker rsync tree krita tmux
